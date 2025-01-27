52a22f37f88a309489f5aabb4d2a401a96aad2b8
siraphoparsarach <47384@kalasinpit.ac.th>
2025-01-27 15:45:14 +0700

    Add files via upload

---
+++ sumNumSeries.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int i = 1,n;
+    scanf("%d" , &n);
+    while(i <= n) {
+       print("%d ", i);
+       i++;
+       sum += i;
+
+    }
+   printf("%d", sum);
+
+ }
